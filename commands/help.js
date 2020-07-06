9484fbd0aa1e0f6c3c898d754049430d9c1a8d98
Kiran Sivakumar <kiranssivakumar@gmail.com>
2020-07-05 23:59:25 -0700

    Update help.js
    boss pls help me pls

--- commands/help.js
+++ commands/help.js
@@ -1,4 +1,12 @@
 const Discord = require('discord.js');
+const fs = require('fs');
+
+commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./').filter(file => file.endsWith('.js') && !file.includes('help.js'));
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	commands.set(command.name, command);
+}
 
 module.exports = {
 	name: 'help',
@@ -6,6 +14,7 @@ module.exports = {
 	description: 'help',
 	cooldown: 10,
 	async execute(message, args) {
-		message.channel.send(`git gud scrub`);
+		helpMsg = "Blonerhelp from Blonerbot:" + commands.reduce((acc, command) => acc + `\n${command.name}: ${command.description} (aliases: ${command.aliases})`)
+		message.channel.send(helpMsg);
 	},
 };
