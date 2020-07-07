57c7f82dff2edfc934f4987959a856d437f78c89
ProfuseJuice <67924716+ProfuseJuice@users.noreply.github.com>
2020-07-06 23:26:32 -0700

    Fix variable declaration

--- commands/help.js
+++ commands/help.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const fs = require('fs');
 
-commands = new Discord.Collection();
+var commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./').filter(file => file.endsWith('.js') && !file.includes('help.js'));
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
@@ -14,7 +14,7 @@ module.exports = {
 	description: 'help',
 	cooldown: 10,
 	async execute(message, args) {
-		helpMsg = "Blonerhelp from Blonerbot:" + commands.reduce((acc, command) => acc + `\n${command.name}: ${command.description} (aliases: ${command.aliases})`)
+		let helpMsg = "Blonerhelp from Blonerbot:" + commands.reduce((acc, command) => acc + `\n${command.name}: ${command.description} (aliases: ${command.aliases})`)
 		message.channel.send(helpMsg);
 	},
 }; 
